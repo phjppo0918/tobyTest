63d76d18a09cb5f8e6b20c6ebc1cb693321e4f24
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-12-29 21:24:26 +0900

    UserDao 내 코드 중복 제거

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -6,8 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby_mysql", "root", "<password>");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
         ps.setString(1, user.getId());
@@ -20,8 +19,7 @@ public class UserDao {
         c.close();
     }
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby_mysql", "root", "<password>");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id  = ?");
         ps.setString(1, id);
@@ -38,5 +36,11 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/toby_mysql", "root", "<password>");
+
+        return c;
     }
 }
