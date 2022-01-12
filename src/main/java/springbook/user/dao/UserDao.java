b097c21ff737eef688521a1e9b36e03ec1b3ea3b
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2022-01-12 15:43:43 +0900

    userDao 불필요한 throws 삭제

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -12,7 +12,7 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
+    public void add(User user) throws SQLException {
         Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
@@ -25,7 +25,7 @@ public class UserDao {
         ps.close();
         c.close();
     }
-    public User get(String id) throws ClassNotFoundException, SQLException {
+    public User get(String id) throws SQLException {
         Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id  = ?");
