2f414a90eccfff46390abbbe66761fe6da8446bd
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2022-01-12 17:08:17 +0900

    UserDao 데이터 전체 삭제 및 데이터 개수 반환 메서드 구현 및 테스트

--- src/test/java/JUnitRunner.java
+++
@@ -1,7 +0,0 @@
-import org.junit.runner.JUnitCore;
-
-public class JUnitRunner {
-    public static void main(String[] args) {
-        JUnitCore.main("springbook.user.dao.UserDaoTest");
-    }
-}
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -21,8 +21,11 @@ public class UserDaoTest {
                 new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
         User user = new User();
-        user.setId("wip");
+        user.setId("dkwip");
         user.setName("test");
         user.setPassword("1234");
 
@@ -32,5 +35,28 @@ public class UserDaoTest {
 
         assertThat(user2.getName(), is(user.getName()));
         assertThat(user2.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("123","1234","132");
+        User user2 = new User("1243","12344","13342");
+        User user3 = new User("13243","12344","13342");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 }
