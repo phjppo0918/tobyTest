646a32ad9a175698c26b59c0dc4596ef3ddbd0b8
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2022-01-14 15:13:33 +0900

    스프링 컨테이너 학습 테스트

--- src/test/java/springbook/learningTest/JUnitTest.java
+++ src/test/java/springbook/learningTest/JUnitTest.java
@@ -1,16 +1,30 @@
 package springbook.learningTest;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.dao.Config;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Config.class)
 public class JUnitTest {
     static JUnitTest testObject;
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+
+    @Autowired
+    ApplicationContext context;
+    static ApplicationContext contextObject = null;
+
     @Test
     public void test1() {
         assertThat(this, is(not(sameInstance(testObject))));
@@ -46,4 +60,30 @@ public class JUnitTest {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
     }
+
+    @Test
+    public void test7() {
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
+    }
+
+    @Test
+    public void test8() {
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
+    }
+
+    @Test
+    public void test9() {
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
+    }
+
+    /*
+    @Test
+    public void test10() {
+        assertThat(contextObject,
+                either(is(nullValue())).or(is(this.context)));
+        contextObject = this.context;
+    }*/
 }
