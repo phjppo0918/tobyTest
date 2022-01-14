a372c60ce5249eedd7707e2517c364c68a3b7ca1
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2022-01-15 00:16:29 +0900

    라인 별로 읽어오는 부분 콜백

--- src/main/java/springbook/callbackSample/Calculator.java
+++ src/main/java/springbook/callbackSample/Calculator.java
@@ -1,6 +1,7 @@
 package springbook.callbackSample;
 
 import springbook.callbackSample.template.BufferedReaderCallback;
+import springbook.callbackSample.template.LineCallback;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,33 +9,25 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-       BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
-           @Override
-           public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-               Integer sum = 0;
-               String line = null;
-               while ((line = br.readLine())!= null) {
-                   sum += Integer.valueOf(line);
-               }
-               return sum;
-           }
-       };
-       return fileReadTemplate(filepath, sumCallback);
+        LineCallback sumCallback =
+                new LineCallback() {
+                    @Override
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value + Integer.valueOf(line);
+                    }
+                };
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
-            @Override
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer multiply = 1;
-                String line = null;
-                while((line = br.readLine()) != null) {
-                    multiply *= Integer.valueOf(line);
-                }
-                return multiply;
-            }
-        };
-        return fileReadTemplate(filepath, multiplyCallback);
+        LineCallback multiplyCallback =
+                new LineCallback() {
+                    @Override
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value * Integer.valueOf(line);
+                    }
+                };
+        return lineReadTemplate(filepath, multiplyCallback, 1);
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
@@ -45,10 +38,10 @@ public class Calculator {
             int ret = callback.doSomethingWithReader(br);
 
             return ret;
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
-        }finally {
+        } finally {
             if (br != null) {
                 try {
                     br.close();
@@ -57,5 +50,32 @@ public class Calculator {
                 }
             }
         }
+    }
+
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+
+            return res;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
     }
 }
---
+++ src/main/java/springbook/callbackSample/template/LineCallback.java
@@ -0,0 +1,5 @@
+package springbook.callbackSample.template;
+
+public interface LineCallback {
+    Integer doSomethingWithLine(String line, Integer value);
+}
