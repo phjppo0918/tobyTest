dd3f9bc62f59dcbd1cde4c85365f52efa3785b88
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2022-01-14 23:33:36 +0900

    Calculator에 try-catch문 적용

--- src/main/java/springbook/callbackSample/Calculator.java
+++ src/main/java/springbook/callbackSample/Calculator.java
@@ -6,15 +6,29 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filepath));
+        BufferedReader br = null;
 
-        Integer sum = 0;
-        String line = null;
-        while((line = br.readLine()) != null) {
-            sum += Integer.valueOf(line);
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+
+            Integer sum = 0;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+
+            return sum;
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-
-        br.close();
-        return sum;
     }
 }
