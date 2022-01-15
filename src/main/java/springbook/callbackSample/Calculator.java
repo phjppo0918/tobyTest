469928fd2f6016e915543f2a9a0367488798967e
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2022-01-15 11:28:28 +0900

    LineCallBack 제네릭스 타입으로 변경

--- src/main/java/springbook/callbackSample/Calculator.java
+++ src/main/java/springbook/callbackSample/Calculator.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        LineCallback sumCallback =
-                new LineCallback() {
+        LineCallback<Integer> sumCallback =
+                new LineCallback<Integer>() {
                     @Override
                     public Integer doSomethingWithLine(String line, Integer value) {
                         return value + Integer.valueOf(line);
@@ -20,14 +20,25 @@ public class Calculator {
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        LineCallback multiplyCallback =
-                new LineCallback() {
+        LineCallback<Integer> multiplyCallback =
+                new LineCallback<Integer>() {
                     @Override
                     public Integer doSomethingWithLine(String line, Integer value) {
                         return value * Integer.valueOf(line);
                     }
                 };
         return lineReadTemplate(filepath, multiplyCallback, 1);
+    }
+
+    public String concatenate(String filepath) throws IOException {
+        LineCallback<String> concatenateCallback =
+                new LineCallback<String>() {
+                    @Override
+                    public String doSomethingWithLine(String line, String value) {
+                        return value + line;
+                    }
+                };
+        return lineReadTemplate(filepath, concatenateCallback, "");
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
@@ -52,12 +63,12 @@ public class Calculator {
         }
     }
 
-    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
         BufferedReader br = null;
 
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer res = initVal;
+            T res = initVal;
             String line = null;
             while ((line = br.readLine()) != null) {
                 res = callback.doSomethingWithLine(line, res);
